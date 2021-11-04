29515e77aa2027a9f692941333c9de6e4a84a66a
Nataliia Cheban <ncheban@f0r3s22.codam.nl>
2021-11-04 17:44:25 +0100

    Check memcpy

--- libft/src/ft_memcpy.c
+++ libft/src/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ncheban <ncheban@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/02 15:39:35 by ncheban       #+#    #+#                 */
-/*   Updated: 2021/11/02 15:39:37 by ncheban       ########   odam.nl         */
+/*   Updated: 2021/11/04 17:43:18 by ncheban       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 void	*ft_memcpy(void	*restrict	dst, const void	*restrict src, size_t n)
 {
-	size_t		i;
-	char		*temp_dst;
-	const char	*temp_src;
+	size_t			i;
+	unsigned char	*temp_dst;
+	unsigned char	*temp_src;
 
+	if (dst == NULL && src == NULL)
+		return (NULL);
 	i = 0;
-	temp_dst = (char *)dst;
-	temp_src = (const char *)src;
-	while (temp_src[i] != '\0' && i < n)
+	temp_dst = (unsigned char *)dst;
+	temp_src = (unsigned char *)src;
+	while (i < n)
 	{
 		temp_dst[i] = temp_src[i];
 		++i;
 	}
-	temp_dst[i] = '\0';
-	return (0);
+	return (dst);
 }
