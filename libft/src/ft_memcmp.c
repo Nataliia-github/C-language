5464ee75c9b1dd7534d0e1c493a0666d6b44b9f5
Nataliia Cheban <ncheban@f0r3s22.codam.nl>
2021-11-06 12:28:53 +0100

    Fix memcmp

--- libft/src/ft_memcmp.c
+++ libft/src/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ncheban <ncheban@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/02 15:39:23 by ncheban       #+#    #+#                 */
-/*   Updated: 2021/11/02 15:39:27 by ncheban       ########   odam.nl         */
+/*   Updated: 2021/11/06 12:28:30 by ncheban       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const char	*s1_1;
-	const char	*s2_1;
-	size_t		i;
+	const unsigned char	*s1_1;
+	const unsigned char	*s2_1;
+	size_t				i;
 
 	i = 0;
-	s1_1 = (const char *)s1;
-	s2_1 = (const char *)s2;
-	if (s1_1 == 0 || s2_1 == 0)
-		return (0);
-	while (s1_1[i] != '\0' && s2_1[i] != '\0' && i < n && s1_1[i] == s2_1[i])
+	s1_1 = (const unsigned char *)s1;
+	s2_1 = (const unsigned char *)s2;
+	while (i < n)
+	{
+		if (s1_1[i] != s2_1[i])
+			return (s1_1[i] - s2_1[i]);
 		++i;
-	return (s1_1[i] - s2_1[i]);
+	}
+	return (0);
 }
