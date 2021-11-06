412392603ca789d40ed2467b12f0e9a9725db7ca
Nataliia Cheban <ncheban@f0r3s22.codam.nl>
2021-11-06 16:02:24 +0100

    strlcpy copy string and find lenght of the src str

--- libft/src/ft_strlcpy.c
+++ libft/src/ft_strlcpy.c
@@ -6,37 +6,26 @@
 /*   By: ncheban <ncheban@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/02 16:03:22 by ncheban       #+#    #+#                 */
-/*   Updated: 2021/11/06 13:29:35 by ncheban       ########   odam.nl         */
+/*   Updated: 2021/11/06 16:00:43 by ncheban       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdio.h>
+#include "libft.h"
 
 size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 {
-	const char	*temp_src;
-	size_t		temp_size;
 	size_t		i;
 
 	i = 0;
-	temp_src = src;
-	temp_size = dstsize;
-	if (dstsize != 0)
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while (i < (dstsize - 1) && src[i] != '\0')
 	{
-		while (--temp_size != 0)
-		{
-			if (dst[i] == 0 && src[i] == 0)
-				break ;
-			++i;
-		}
+		dst[i] = src[i];
+		++i;
 	}
-	if (temp_size == 0)
-	{
-		if (dstsize != 0)
-			*dst = '\0';
-		while (*src != 0)
-			++src;
-	}
-	return (src - temp_src);
+	dst[i] = '\0';
+	return (ft_strlen(src));
 }
