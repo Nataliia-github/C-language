7648bd0773398f396117f89475cab8b5f46c670a
Nataliia Cheban <ncheban@f0r3s22.codam.nl>
2021-11-06 15:14:15 +0100

    Only removed NULL protection in strncmp, i need segmentation faul

--- libft/src/ft_strncmp.c
+++ libft/src/ft_strncmp.c
@@ -6,20 +6,23 @@
 /*   By: ncheban <ncheban@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/02 16:06:23 by ncheban       #+#    #+#                 */
-/*   Updated: 2021/11/02 16:06:25 by ncheban       ########   odam.nl         */
+/*   Updated: 2021/11/06 15:12:54 by ncheban       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <string.h>
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	if (s1 == 0 || s2 == 0)
-		return (0);
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n && s1[i] == s2[i])
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		++i;
-	return (s1[i] - s2[i]);
+	}
+	return (0);
 }
