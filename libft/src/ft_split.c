63ce97b436c51655ae5c950fe51fbc984532a81f
Nataliia Cheban <ncheban@f1r1s18.codam.nl>
2021-11-09 12:03:11 +0100

    remove unit-test

--- libft/src/ft_split.c
+++ libft/src/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: nataliya <nataliya@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/08 17:47:27 by nataliya      #+#    #+#                 */
-/*   Updated: 2021/11/08 18:08:57 by nataliya      ########   odam.nl         */
+/*   Updated: 2021/11/09 11:57:35 by ncheban       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,38 @@ int	ft_count_words(char const *s, char c)
 	return (words);
 }
 
-char **ft_in_split(char **res, char const *s, int words)
+ void	ft_in_split(char **res, char const *s, int c, int words)
 {
-	
+	int		i;
+	int		j;
+	int		s_len;
+	char	*str;
+
+	i = 0;
+	s_len = ft_strlen(s);
+	while (s_len > 0)
+	{
+		while (s[s_len] == c)
+			--s_len;
+		while (s[s_len] != c)
+		{
+			str[i] == s[s_len];
+			++i;
+			--s_len;
+		}
+		j = 0;
+		res[words] = malloc((i + 1) * sizeof(char));
+		while (i > 0)
+		{	
+			res[words][j] = str[i];
+			++i;
+			++j;
+		}
+		res[words][j] = '\0';
+		--words;
+		--s_len;
+	}
+	return (0);
 }
 
 char	**ft_split(char const *s, char c)
@@ -37,13 +66,13 @@ char	**ft_split(char const *s, char c)
 	char	**res;
 	int		words;
 
-	if (s == NULL || c == 0)
+	if (s == NULL || c == '\0' || c == 0)
 		return (NULL);
 	words = ft_count_words (s, c);
 	res = (char	**)malloc((words + 1) * sizeof(char *));
 	if (res == NULL)
 		return (NULL);
 	res[words] = 0;
-	res = ft_in_split(res, s, words);
+	ft_in_split(res, s, c, words);
 	return(res);
 }
