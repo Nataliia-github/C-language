e6b8bd7399a14121aa7b03aa0c79dd2c33c927e0
Nataliia Cheban <n.cheban@gmail.com>
2021-11-10 19:30:36 +0100

    delete comments in substr

--- libft/src/ft_substr.c
+++ libft/src/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nataliya <nataliya@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/07 10:43:05 by nataliya      #+#    #+#                 */
-/*   Updated: 2021/11/10 19:15:03 by nataliya      ########   odam.nl         */
+/*   Updated: 2021/11/10 19:29:16 by nataliya      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,36 +37,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	substr[i] = '\0';
 	return (substr);
 }
-
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	char			*substr;
-	
-// 	substr = malloc((len + 1) * sizeof(char));
-// 	if (substr == NULL)
-// 		return (NULL);
-// 	if (ft_strlen(s) >= start)
-// 		substr[0] = '\0';
-// 	else
-// 	{
-// 		ft_memcpy(substr, (s + start), len);
-// 		substr[len] = '\0';
-// 	}	
-// 	return (substr);
-// }
-
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	unsigned char		*sub;
-// 	sub = malloc(sizeof(char) * (len + 1));
-// 	if (!sub)
-// 		return (NULL);
-// 	if (start >= ft_strlen(s))
-// 		*sub = '\0';
-// 	else
-// 	{
-// 		ft_memcpy(sub, s + start, len);
-// 		*(sub + len) = '\0';
-// 	}
-// 	return ((char *)sub);
-// }
