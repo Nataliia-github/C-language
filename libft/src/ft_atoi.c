405ef348783f41f8afc3baf0bbaa0ba1e6a59219
Nataliia Cheban <ncheban@f0r1s15.codam.nl>
2021-11-14 12:42:41 +0100

    ft_atoi - norm

--- libft/src/ft_atoi.c
+++ libft/src/ft_atoi.c
@@ -10,10 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/* convert a string to an integer. */
+
 int	ft_skip_white(const char *str, int i)
 {
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\r'
 		|| str[i] == '\n' || str[i] == '\f' || str[i] == '\v')
+		++i;
+	return (i);
+}
+
+int	ft_is_neg(const char *str, int i)
+{
+	int	neg;
+
+	neg = 1;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		++i;
+	}
+	return (neg);
+}
+
+int	ft_skip_pm(const char *str, int i)
+{
+	while (str[i] == '+' || str[i] == '-')
 		++i;
 	return (i);
 }
@@ -26,22 +49,17 @@ int	ft_atoi(const char *str)
 	int	num;
 
 	i = 0;
-	neg = 1;
 	num = 0;
 	j = 0;
 	i = ft_skip_white(str, i);
-	while (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			neg *= -1;
-		++i;
-	}
+	neg = ft_is_neg(str, i);
+	i = ft_skip_pm(str, i);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		num = num * 10 + (str[i] - '0');
 		if (j > 10 && neg == 1)
 			return (-1);
-		else if (j > 11 && neg == -1)
+		else if (j > 10 && neg == -1)
 			return (0);
 		++i;
 		++j;
