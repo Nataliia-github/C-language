8a0288a43692399e76a59297c9df9510e7e87216
Nataliia Cheban <n.cheban@gmail.com>
2021-11-22 17:38:06 +0100

    lstlast is finished

---
+++ libft/ft_lstlast.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstlast.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nataliya <nataliya@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/11/22 17:22:38 by nataliya      #+#    #+#                 */
+/*   Updated: 2021/11/22 17:33:58 by nataliya      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	while (lst != NULL)
+	{
+		if (lst->next == NULL)
+			return (lst);
+		lst = lst->next;
+	}
+	return (lst);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ncheban <ncheban@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 16:56:34 by ncheban       #+#    #+#                 */
-/*   Updated: 2021/11/21 21:19:24 by ncheban       ########   odam.nl         */
+/*   Updated: 2021/11/22 17:24:10 by nataliya      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,5 +72,6 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
