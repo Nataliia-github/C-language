204a990db59bded88bae9f0902517ea1dc82e420
Nataliia Cheban <n.cheban@gmail.com>
2021-11-22 18:04:19 +0100

    lstadd_back is finished

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstadd_back.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nataliya <nataliya@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/11/22 17:39:17 by nataliya      #+#    #+#                 */
+/*   Updated: 2021/11/22 18:02:28 by nataliya      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (*lst != NULL)
+		ft_lstlast(*lst)->next = new;
+	else
+	*lst = new;
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ncheban <ncheban@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 16:56:34 by ncheban       #+#    #+#                 */
-/*   Updated: 2021/11/22 17:24:10 by nataliya      ########   odam.nl         */
+/*   Updated: 2021/11/22 17:56:17 by nataliya      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,5 +73,6 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
