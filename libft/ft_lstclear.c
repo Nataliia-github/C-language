67448de1538b04cd07ca602f0c7e95acb7aed6d9
Nataliia Cheban <ncheban@f0r3s25.codam.nl>
2021-11-23 14:44:09 +0100

    lstclear is finished

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: ncheban <ncheban@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 11:57:17 by ncheban       #+#    #+#                 */
-/*   Updated: 2021/11/23 11:58:29 by ncheban       ########   odam.nl         */
+/*   Updated: 2021/11/23 14:39:20 by ncheban       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list	*temp_lst;
+	t_list	*temp_next;
 	
+	if (del == NULL || *lst == NULL || lst == NULL)
+		return ;
+	temp_lst = *lst;
+	while (temp_lst != NULL)
+	{
+		temp_next = temp_lst->next;
+		ft_lstdelone(temp_lst, del);
+		temp_lst = temp_next;
+	}
+	*lst = NULL;
 }
