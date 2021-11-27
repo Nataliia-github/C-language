3bc7bb9ec01527e8b85f66efe12c6cbd4323242d
Nataliia Cheban <ncheban@f0r1s8.codam.nl>
2021-11-27 19:13:18 +0100

    finish og the working day

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,29 +6,24 @@
 /*   By: ncheban <ncheban@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/21 12:00:07 by ncheban       #+#    #+#                 */
-/*   Updated: 2021/11/27 16:34:43 by ncheban       ########   odam.nl         */
+/*   Updated: 2021/11/27 18:52:42 by ncheban       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdarg.h>
 #include "../libft/libft.h"
-#include "ft_printf.h"
+// #include "ft_printf.h"
 
 int	ft_printf(const char *restrict input_str, ...)
 {
-	va_list	arg_valist;
-//	va_list	cp_arg;
-	int		done;
+	va_list	arg_ptr;
+	int		i;
 
-	va_start(arg_valist, input_str);
-//	cp_arg = 0;
-	done = 0;
-	while (va_arg (arg_valist, char *))
-		printf("%s\n", (char *)arg_valist);
-	ft_putstr_fd((char	*)input_str, 1);
-	// ft_putstr_fd(va_arg (arg_valist, char	*), 1);
-	// ft_putstr_fd(va_arg (arg_valist, char	*), 1);
-	va_end(arg_valist);
-	return (done);
+	va_start(arg_ptr, input_str);
+	printf ("%s\n", input_str);
+	i = 0;
+	printf ("%i\n", i);
+	va_end(arg_ptr);
+	return (0);
 }
--- ft_printf/test_ft_printf.c
+++ ft_printf/test_ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ncheban <ncheban@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/27 12:55:28 by ncheban       #+#    #+#                 */
-/*   Updated: 2021/11/27 13:01:18 by ncheban       ########   odam.nl         */
+/*   Updated: 2021/11/27 18:38:11 by ncheban       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 
 int	main(void)
 {
-	ft_printf("bla-bla-bla\n", 9, 10);
+	ft_printf("%i -> %i -> %i", 55, 56, 57);
 	return (0);
 }
