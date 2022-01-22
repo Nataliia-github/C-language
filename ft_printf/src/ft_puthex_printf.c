747951b55d791505bad8748d2e38bbbfa1f0e9ac
Nataliia Cheban <n.cheban@gmail.com>
2022-01-22 18:28:30 +0100

    delete trash from puthex

--- ft_printf/src/ft_puthex_printf.c
+++ ft_printf/src/ft_puthex_printf.c
@@ -6,49 +6,12 @@
 /*   By: ncheban <ncheban@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/07 14:37:02 by ncheban       #+#    #+#                 */
-/*   Updated: 2022/01/22 18:20:36 by nataliya      ########   odam.nl         */
+/*   Updated: 2022/01/22 18:27:54 by nataliya      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "ft_printf.h"
-
-// // static int	ft_puthex(unsigned long long hex, int print_len, int fd, char upper)
-// // {
-// // 	if (hex >= 16)
-// // 	{
-// // 		ft_puthex(hex / 16, ++print_len, fd, upper);
-// // 		ft_puthex(hex % 16, ++print_len, fd, upper);
-// // 	}
-// // 	else
-// // 	{
-// // 		if (hex < 10)
-// // 		{
-// // 			if (upper == 'X')
-// // 				print_len += ft_putchar_printf(ft_toupper(hex + '0'), fd);
-// // 			else
-// // 				print_len += ft_putchar_printf((hex + '0'), fd);
-// // 		}
-// // 		else
-// // 		{
-// // 			// if (upper == 'X')
-// // 			// 	print_len += ft_putchar_printf(ft_toupper(hex - 10 + 'a'), fd);
-// // 			// else
-// // 			print_len += ft_putchar_printf((hex - 10 + 'a'), fd);
-// // 			printf("%llu\n",(hex - 10 + '0'));
-// // 		}	
-// // 	}
-// // 	return (print_len);
-// // }
-
-// // int	ft_puthex_printf(unsigned long long hex, int fd, char upper)
-// // {
-// // 	int		print_len;
-
-// // 	print_len = 0;
-// // 	print_len += ft_puthex(hex, print_len, fd, upper);
-// // 	return (print_len);
-// // }
 
 static int	ft_count_size(unsigned long long hex)
 {
